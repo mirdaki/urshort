90c214c04a4c9d68dc9978bd6137aa6551486e67
mirdaki <mirdaki@users.noreply.github.com>
2021-12-12 17:11:23 -0800

    Vanity and regex with warp

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # URShort
 
 A blazingly fast and amazingly simple URL shortner designed for use by a single person.
+
+Steps:
+1. [X] Get a simple redirect working
+2. [X] Work with multiple simple redirects
+3. [ ] Load multiple simple redirects from file
+4. [X] Get pattern redirects working
+5. [ ] Load multiple pattern from file
+- [ ] Make 404 fallback
+6. [X] Get short URL request working
+    - Vanity, pattern, or consistent hash
+    - ~~Limit to allowed domains (both the hashes and the requester)~~
+7. [ ] Support http -> https
+8. [ ] Support rate limiting
+9. ~~[ ] Add telemetry option~~
+10. ~~[ ] Store data in database~~
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,273 @@
-#![deny(warnings)]
+use std::{collections::HashMap, convert::Infallible, str::FromStr};
 
-use std::convert::Infallible;
+use regex::Regex;
 
-use hyper::service::{make_service_fn, service_fn};
-use hyper::{Body, Request, Response, Server};
-
-async fn hello(_: Request<Body>) -> Result<Response<Body>, Infallible> {
-    Ok(Response::new(Body::from("Hello World!")))
-}
+use warp::{http::Uri, redirect, reject, Filter};
 
 #[tokio::main]
-pub async fn main() -> Result<(), Box<dyn std::error::Error + Send + Sync>> {
-    // For every connection, we must make a `Service` to handle all
-    // incoming HTTP requests on said connection.
-    let make_svc = make_service_fn(|_conn| {
-        // This is the `Service` that will handle the connection.
-        // `service_fn` is a helper to convert a function that
-        // returns a Response into a `Service`.
-        async { Ok::<_, Infallible>(service_fn(hello)) }
-    });
+async fn main() {
+    let mut urls = UrlMappings::new();
+    urls.vanity = HashMap::from([
+        ("i".to_string(), Uri::from_str("https://codecaptured.com").unwrap()),
+        (
+            "i5".to_string(),
+            Uri::from_str("https://codecaptured.com/five").unwrap(),
+        ),
+    ]);
+    urls.pattern = vec![
+        (
+            Regex::new(r"^(?P<index>\d+)$").unwrap(),
+            "https://codecaptured.com/$index".to_string(),
+        ),
+        (
+            Regex::new(r"^i(?P<index>\d+)$").unwrap(),
+            "https://codecaptured.com/$index".to_string(),
+        ),
+    ];
+    // vanity_urls.load_from_file();
 
-    let addr = ([127, 0, 0, 1], 3000).into();
+    // `Get /` Load the root message to inform this is live
+    let root_message = warp::path::end().and(warp::get()).and_then(get_root);
 
+    // `Get /:path` Attempt to redirect to a URL
+    let short_url = warp::path::param()
+        .and(warp::get())
+        .and(with_url_mappings(urls))
+        .and_then(get_match)
+        .or(warp::get())
+        .and_then(get_not_found);
+
+    let routes = warp::any().and(root_message.or(short_url));
+
+    //println!("\nRust Warp Server ready at {}", blue.apply_to(&target));
+    println!("Use $curl 0.0.0.0:8000/hello/www.steadylearner.com to test the end point.");
+
+    /* let addr = ([127, 0, 0, 1], 3000).into();
     let server = Server::bind(&addr).serve(make_svc);
+    println!("Listening on http://{}", addr); */
 
-    println!("Listening on http://{}", addr);
+    warp::serve(routes).run(([127, 0, 0, 1], 3030)).await;
+}
 
-    server.await?;
+async fn get_root() -> Result<impl warp::Reply, Infallible> {
+    Ok("URShort is running!") // TODO: Project name from cargo? or const
+}
 
-    Ok(())
+async fn get_match(path: String, urls: UrlMappings) -> Result<impl warp::Reply, warp::Rejection> {
+    match urls.match_anything(&path) {
+        Ok(x) => Ok(redirect(x)),
+        Err(_) => Err(reject::not_found()),
+    }
+}
+
+async fn get_not_found() -> Result<impl warp::Reply, Infallible> {
+    Ok("URShort is running!")
+}
+
+fn with_url_mappings(
+    urls: UrlMappings,
+) -> impl Filter<Extract = (UrlMappings,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || urls.clone())
+}
+
+/* async fn get_url(path: String, vanity_urls: Vanity) -> Result<impl warp::Reply, warp::Rejection> {
+    match vanity_urls.urls.get(&path) {
+        Some(x) => Ok(redirect(x.clone())),
+        None => Err(reject::not_found()),
+    }
+}
+
+fn with_vanity(
+    urls: Vanity,
+) -> impl Filter<Extract = (Vanity,), Error = std::convert::Infallible> + Clone {
+    warp::any().map(move || urls.clone())
+} */
+
+/* #[derive(Clone)]
+struct Vanity {
+    urls: HashMap<String, Uri>,
+}
+
+impl Vanity {
+    pub fn new() -> Vanity {
+        Vanity {
+            urls: HashMap::new(),
+        }
+    }
+
+    pub fn load_from_memory(urls: HashMap<String, Uri>) -> Vanity {
+        Vanity { urls }
+    }
+
+    pub fn load_from_file(&mut self /*, file: file()*/) {
+        self.urls.insert(
+            "1".to_string(),
+            Uri::from_static("https://codecaptured.com"),
+        );
+        self.urls.insert(
+            "bye".to_string(),
+            Uri::from_static("http://matthewbooe.com"),
+        );
+    }
+} */
+
+#[derive(Clone)]
+struct UrlMappings {
+    vanity: HashMap<String, Uri>,
+    pattern: Vec<(Regex, String)>,
+}
+
+impl UrlMappings {
+    pub fn new() -> UrlMappings {
+        UrlMappings {
+            vanity: HashMap::new(),
+            pattern: Vec::new(),
+        }
+    }
+
+    pub fn match_vanity(&self, uri: &str) -> Result<Uri, &str> {
+        match self.vanity.get(uri) {
+            Some(x) => Ok(x.clone()),
+            None => Err("No vanity found"),
+        }
+    }
+
+    pub fn match_pattern(&self, uri: &str) -> Result<Uri, &str> {
+        for (regex, uri_pattern) in &self.pattern {
+            if !regex.is_match(uri) {
+                continue;
+            }
+
+            let replacement = regex.replace(uri, uri_pattern);
+
+            return match Uri::from_str(&replacement) {
+                Ok(new_uri) => Ok(new_uri),
+                Err(_) => Err("Pattern did not create URI"),
+            };
+        }
+
+        Err("No pattern found")
+    }
+
+    pub fn match_anything(&self, uri: &str) -> Result<Uri, &str> {
+        match self.match_vanity(uri) {
+            Ok(vanity) => Ok(vanity),
+            Err(_) => self.match_pattern(uri),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use std::str::FromStr;
+
+    use warp::http::uri::InvalidUri;
+
+    use super::*;
+
+    #[test]
+    fn vanity_urls() -> Result<(), InvalidUri> {
+        let mut urls = UrlMappings::new();
+        urls.vanity = HashMap::from([
+            ("test".to_string(), Uri::from_str("https://example.com")?),
+            ("1/1".to_string(), Uri::from_str("https://example.com/1")?),
+            (
+                "3.14".to_string(),
+                Uri::from_str("https://example.com/pi")?,
+            ),
+        ]);
+
+        // No matches
+        assert!(urls.match_vanity("/invalid").is_err());
+
+        // Can't match an invalid URI, because it must be a URI to be loaded into the hashmap
+
+        // Vanity matches
+        assert_eq!(
+            urls.match_vanity("test").unwrap(),
+            Uri::from_str("https://example.com")?
+        );
+        assert_eq!(
+            urls.match_vanity("1/1").unwrap(),
+            Uri::from_str("https://example.com/1")?
+        );
+        assert_eq!(
+            urls.match_vanity("3.14").unwrap(),
+            Uri::from_str("https://example.com/pi")?
+        );
+
+        Ok(())
+    }
+
+    #[test]
+    fn pattern_urls() -> Result<(), InvalidUri> {
+        let mut urls = UrlMappings::new();
+        urls.pattern = vec![
+            (
+                Regex::new(r"(?P<last>[^,\s]+),\s+(?P<first>\S+)").unwrap(),
+                "$first $last".to_string(),
+            ),
+            (
+                Regex::new(r"^i(?P<index>\d+)$").unwrap(),
+                "https://example.com/$index".to_string(),
+            ),
+        ];
+
+        // Pattern is close, but does not match
+        assert!(urls.match_pattern("i12.12").is_err());
+        assert!(urls.match_pattern("i-1212").is_err());
+        assert!(urls.match_pattern("i1212g").is_err());
+        assert!(urls.match_pattern("-i1212g").is_err());
+
+        // Pattern matches, but not URI
+        assert!(urls.match_pattern("Solo, Jaina").is_err());
+
+        // Pattern matches and is URI
+        let result = urls.match_pattern("i1212");
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), Uri::from_str("https://example.com/1212")?);
+
+        Ok(())
+    }
+
+    #[test]
+    fn vanity_and_pattern_urls() -> Result<(), InvalidUri> {
+        let mut urls = UrlMappings::new();
+        urls.vanity = HashMap::from([
+            ("i".to_string(), Uri::from_str("https://example.com")?),
+            (
+                "i5".to_string(),
+                Uri::from_str("https://example.com/five")?,
+            ),
+            (
+                "unrelated".to_string(),
+                Uri::from_str("https://example.com/byebye")?,
+            ),
+        ]);
+        urls.pattern = vec![
+            (
+                Regex::new(r"^(?P<index>\d+)$").unwrap(),
+                "https://example.com/$index".to_string(),
+            ),
+            (
+                Regex::new(r"^i(?P<index>\d+)$").unwrap(),
+                "https://example.com/$index".to_string(),
+            ),
+        ];
+        // No match at all
+        assert!(urls.match_anything("ithree").is_err());
+        assert!(urls.match_anything("bad").is_err());
+
+        // Vanity matches are preferred over pattern matches
+        let result = urls.match_anything("i5");
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), Uri::from_str("https://example.com/five")?);
+
+        // Pattern match used when no vanity
+        let result = urls.match_anything("i42");
+        assert!(result.is_ok());
+        assert_eq!(result.unwrap(), Uri::from_str("https://example.com/42")?);
+
+        Ok(())
+    }
 }
